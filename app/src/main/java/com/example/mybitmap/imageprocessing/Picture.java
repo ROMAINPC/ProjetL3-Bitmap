cca8f61cfe5ab5809b43f3312065ec4491e64fc8
romainpc <romainpc.lechat@laposte.net>
2019-11-07 15:47:00 +0100

    ok

---
+++ app/src/main/java/com/example/mybitmap/imageprocessing/Picture.java
@@ -0,0 +1,239 @@
+package com.example.mybitmap.imageprocessing;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+
+import java.util.HashMap;
+
+/**
+ * Class to manage an Picture, this class wrap a Bitmap instance and several others informations about the image.
+ */
+public class Picture {
+
+    public enum Histogram {
+        LUMINANCE,
+        GRAY_LEVEL_NATURAL
+    }
+
+
+    private Bitmap bitmap;
+    private BitmapFactory.Options options;
+    private int src;
+    private Context ctx;
+
+    private int width;
+    private int height;
+    private int sourceWidth;
+    private int sourceHeight;
+    private int[] original;
+
+    private HashMap<Histogram, int[]> histograms = new HashMap<>();
+
+    /**
+     * Just copy a Picture instance.
+     *
+     * @param pic
+     * @deprecated
+     */
+    public Picture(Picture pic) {
+        this(pic, 0, 0);
+    }
+
+    /**
+     * Picture constructor by copying another Picture.
+     *
+     * @param pic          Picture to copy.
+     * @param newReqHeight New size of the new picture.
+     * @param newReqWidth  New size of the new picture.
+     * @deprecated Still small unit bug, don't use, there are risks when using "reset" after.
+     */
+    public Picture(Picture pic, int newReqWidth, int newReqHeight) {
+        this.src = pic.getSource();
+        this.ctx = pic.ctx;
+        options = new BitmapFactory.Options();
+
+        //generate empty bitmap:
+        sourceHeight = pic.getsourceHeight();
+        sourceWidth = pic.getsourceWidth();
+
+        int sampleRatio = Utils.calculateInSampleSize(sourceWidth, sourceHeight, newReqWidth, newReqHeight);
+        height = sourceHeight / sampleRatio;
+        width = sourceWidth / sampleRatio;
+
+        //generate bitmap:
+        bitmap = Bitmap.createScaledBitmap(pic.getBitmap(), pic.getBitmap().getWidth() / sampleRatio, pic.getBitmap().getHeight() / sampleRatio, true);
+
+        original = new int[bitmap.getWidth() * bitmap.getHeight()];
+        quickSave();
+    }
+
+
+    /**
+     * Picture constuctor.
+     *
+     * @param ctx Context in which the Picture evolve.
+     * @param src Resource identifier of the picture.
+     */
+    public Picture(Context ctx, int src) {
+        this(ctx, src, 0, 0);
+    }
+
+    /**
+     * Picture constuctor.
+     *
+     * @param ctx       Context in which the Picture evolve.
+     * @param src       Resource identifier of the picture.
+     * @param reqWidth  Required width, usefull to reduce the size of the loaded image.
+     * @param reqHeight Required height.
+     */
+    public Picture(Context ctx, int src, int reqWidth, int reqHeight) {
+        this.src = src;
+        this.ctx = ctx;
+        options = new BitmapFactory.Options();
+        options.inMutable = true;
+
+        //first decode:
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(ctx.getResources(), src, options);
+
+        sourceHeight = options.outHeight;
+        sourceWidth = options.outWidth;
+        options.inSampleSize = Utils.calculateInSampleSize(sourceWidth, sourceHeight, reqWidth, reqHeight);
+
+
+        //final decode:
+        options.inJustDecodeBounds = false;
+        bitmap = BitmapFactory.decodeResource(ctx.getResources(), src, options);
+        height = options.outHeight;
+        width = options.outWidth;
+
+        //save original:
+        original = new int[bitmap.getWidth() * bitmap.getHeight()];
+        quickSave();
+    }
+
+    /**
+     * Reset all pixels to the last quicksave values, keeping required dimensions.
+     */
+    public void reset() {
+        bitmap.setPixels(original, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+    }
+
+    /**
+     * Save the current state of the picture (erase last quicksave)
+     */
+    public void quickSave() {
+        bitmap.getPixels(original, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+    }
+
+    /**
+     * Similar to reset() but re read source file to fully reset the picture, even though there have been quicksaves.
+     */
+    public void reload() {
+
+        //first decode:
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(ctx.getResources(), src, options);
+        //no modification on options.inSampleSize;
+
+        //final decode:
+        options.inJustDecodeBounds = false;
+        options.inMutable = true;
+        bitmap = BitmapFactory.decodeResource(ctx.getResources(), src, options);
+
+        //resave original:
+        quickSave();
+    }
+
+    /**
+     * @return Width in pixels.
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * @return Height in pixels.
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * @return Original file width in pixels.
+     */
+    public int getsourceWidth() {
+        return sourceWidth;
+    }
+
+    /**
+     * @return Original file height in pixels.
+     */
+    public int getsourceHeight() {
+        return sourceHeight;
+    }
+
+    /**
+     * @return Bitmap associated to the picture.
+     */
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    /**
+     * @return R source integer associated to the picture.
+     */
+    public int getSource() {
+        return src;
+    }
+
+    /**
+     * To know if a precise type of histogram was genereate for this picture.
+     *
+     * @param type Histogram type
+     * @return true if generated.
+     */
+    public boolean isHistogramGenerated(Histogram type) {
+        return histograms.containsKey(type);
+    }
+
+    /**
+     * @param type The type of the selected histogram.
+     * @return Integer array (256 values) corresponding to the histogram.
+     */
+    public int[] getHistogram(Histogram type) {
+        return histograms.get(type);
+    }
+
+
+    /**
+     * Compute an histogram.
+     *
+     * @param type The histogram type.
+     */
+    public void generateHistogram(Histogram type) {
+        int[] histogram = new int[256];
+        int[] pixels = new int[bitmap.getWidth() * bitmap.getHeight()];
+        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < pixels.length; i++) {
+            //compute histograms, depending the historgram type:
+            int px = pixels[i];
+            switch (type) {
+                case LUMINANCE:
+                    float[] hsv = new float[3];
+                    Utils.RGBToHSV(Color.red(px), Color.green(px), Color.blue(px), hsv);
+                    histogram[(int) (hsv[2] * 255f)]++;
+                    break;
+                case GRAY_LEVEL_NATURAL:
+                    int gray = (int) (0.3 * (double) Color.red(px) + 0.59 * (double) Color.blue(px) + 0.11 * (double) Color.green(px));
+                    histogram[gray]++;
+                    break;
+            }
+        }
+
+        histograms.put(type, histogram);
+    }
+
+}
---
+++ app/src/main/java/com/example/mybitmap/imageprocessing/Utils.java
@@ -0,0 +1,171 @@
+package com.example.mybitmap.imageprocessing;
+
+import android.graphics.Color;
+
+/**
+ * Class with tools for image processing.
+ *
+ * @author Romain Pigret-Cadou
+ */
+public class Utils {
+
+
+    /**
+     * Tool to convert a HSV color contain in a float array to a RGB int value.
+     * Also need alpha value, transmitted to the RGB color.
+     * hsv[0] -> H:[0;360]
+     * hsv[1] -> S:[0;1]
+     * hsv[2] -> V:[0;1]
+     *
+     * @param hsv
+     * @param alpha
+     * @return the ARGB color.
+     * @see android.graphics.Color
+     */
+    public static int HSVToColor(float[] hsv, int alpha) {
+        //use conversion describe at https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+        int t = (int) ((hsv[0] / 60f) % 6);
+        float C = hsv[1] * hsv[2];
+        float X = C * (1 - Math.abs(((hsv[0] / 60) % 2) - 1));
+        float m = hsv[2] - C;
+
+        float r = 0;
+        float g = 0;
+        float b = 0;
+
+        switch (t) {
+            case 0:
+                r = C;
+                g = X;
+                b = 0;
+                break;
+            case 1:
+                r = X;
+                g = C;
+                b = 0;
+                break;
+            case 2:
+                r = 0;
+                g = C;
+                b = X;
+                break;
+            case 3:
+                r = 0;
+                g = X;
+                b = C;
+                break;
+            case 4:
+                r = X;
+                g = 0;
+                b = C;
+                break;
+            case 5:
+                r = C;
+                g = 0;
+                b = X;
+                break;
+        }
+        float r2 = (r + m) * 255;
+        float g2 = (g + m) * 255;
+        float b2 = (b + m) * 255;
+        return Color.argb(alpha, (int) r2, (int) g2, (int) b2);
+    }
+
+    /**
+     * Tool to convert RGB Color to HSV Color contain in a float array.
+     * NB: Doesn't use alpha value.
+     *
+     * @param red   Red integer value in [0;255]
+     * @param green Green integer value in [0;255]
+     * @param blue  Blue integer value in [0;255]
+     * @param hsv   Modified, contain at the end the HSV value : hsv[0] -> H:[0;360] / hsv[1] -> S:[0;1] / hsv[2] -> V:[0;1]
+     */
+    public static void RGBToHSV(int red, int green, int blue, float[] hsv) {
+        //use conversion describe at https://fr.wikipedia.org/wiki/Teinte_Saturation_Valeur
+        float r = red / 255f;
+        float g = green / 255f;
+        float b = blue / 255f;
+
+        float max = Math.max(Math.max(r, g), b);
+        float min = Math.min(Math.min(r, g), b);
+
+        if (max == min) {
+            hsv[0] = 0;
+        } else if (max == r) {
+            hsv[0] = (60 * ((g - b) / (max - min)) + 360);
+            while (hsv[0] > 360) hsv[0] -= 360;
+        } else if (max == g) {
+            hsv[0] = (60 * ((b - r) / (max - min)) + 120);
+        } else if (max == b) {
+            hsv[0] = (60 * ((r - g) / (max - min)) + 240);
+        }
+
+        if (max == 0) {
+            hsv[1] = 0;
+        } else {
+            hsv[1] = 1 - (min / max);
+        }
+
+        hsv[2] = max;
+
+    }
+
+    /**
+     * Function to calculate integer to load sample of an image.
+     * See <a href="https://developer.android.com/topic/performance/graphics/load-bitmap">Loading large bitmaps</a>
+     *
+     * @param originalWidth  Width of the orignal picture.
+     * @param originalHeight Height of the orignal picture.
+     * @param reqWidth       The desired width for the sample.
+     * @param reqHeight      The desired width for the sample.
+     * @return An integer corresponding to the nearest power of 2 near X, so it takes X*X pixels to make 1 pixel in the sample.
+     */
+    public static int calculateInSampleSize(int originalWidth, int originalHeight,
+                                            int reqWidth, int reqHeight) {
+        if (reqWidth <= 0 && reqHeight <= 0)
+            return 1;
+        // Raw height and width of image
+        final int height = originalHeight;
+        final int width = originalWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    /**
+     * Compute minimum and maximum value in an histogram array.
+     *
+     * @param histogram
+     * @return An array with minimum value at index 0 and maximum at index 1.
+     */
+    public static int[] getHistogramMinMaxValue(int[] histogram) {
+        int min;
+        int max;
+
+        int i = 0;
+        while (histogram[i] == 0) {
+            i++;
+        }
+        min = i;
+        i = histogram.length - 1;
+        while (histogram[i] == 0) {
+            i--;
+        }
+        max = i;
+        return new int[]{min, max};
+    }
+
+
+}
